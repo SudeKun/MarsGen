317577b8aab6542d6486e05f40ab3bf53e603b7c
Sude <sudekunduroglu@gmail.com>
2022-11-21 11:41:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarsGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
