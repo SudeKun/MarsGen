9df78568ac1c9a1f4b9b863b2b73bc8cb085d615
Sude <sudekunduroglu@gmail.com>
2022-11-28 10:39:17 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +7,137 @@ using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.ComponentModel.Design;
 using System.Drawing;
+using System.Reflection;
 
 namespace MarsGen
 {
     internal class Program
     {
+        static string random_dna()
+        {
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            string[] Ala = { "GCT", "GCC", "GCA", "GCG" };
+            string[] Arg = { "CGT", "CGC", "CGA", "CGG", "AGA", "AGG" };
+            string[] Asn = { "AAT", "AAC" };
+            string[] Asp = { "GAT", "GAC" };
+            string[] Cys = { "TGT", "TGC" };
+            string[] Gln = { "CAA", "CAG" };
+            string[] Glu = { "GAA", "GAG" };
+            string[] Gly = { "GGT", "GGC", "GGA", "GGG" };
+            string[] His = { "CAT", "CAC" };
+            string[] Ile = { "ATT", "ATC", "ATA" };
+            string[] Leu = { "CTT", "CTC", "CTA", "CTG", "TTA", "TTG" };
+            string[] Lys = { "AAA", "AAG" };
+            string[] Met = { "ATG" }; //START
+            string[] Phe = { "TTT", "TTC" };
+            string[] Pro = { "CCT", "CCC", "CCA", "CCG" };
+            string[] Ser = { "TCT", "TCC", "TCA", "TCG", "AGT", "AGC" };
+            string[] Thr = { "ACT", "ACC", "ACA", "ACG" };
+            string[] Trp = { "TGG" };
+            string[] Tyr = { "TAT", "TAC" };
+            string[] Val = { "GTT", "GTC", "GTA", "GTG" };
+            string[] Stp = { "TAA", "TGA", "TAG" }; //STOP
+            string[] gender = { Lys[0], Phe[0], Gly[3], Pro[1] };
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            
+            string mydna;
+            Random random = new Random();
+            random.Next(0, 5);
+            Console.WriteLine("Please enter the gender operation you want ");
+            string op = Console.ReadLine().ToLower();
+            mydna = "AGT" + gender[random.Next(0, gender.Length)] + gender[random.Next(0, gender.Length)] + Stp[random.Next(0, Stp.Length)];
+            char[] my_dna = mydna.ToCharArray();
+            bool flag2 = true;
+            if (op == "female" || op == "f")
+            {
+                while (flag2)
+                {
+                    if (!(((my_dna[4] == 'A') || (my_dna[4] == 'T')) && ((my_dna[7] == 'A') || (my_dna[7] == 'T'))))
+                    {
+                        mydna = "AGT" + gender[random.Next(0, gender.Length)] + gender[random.Next(0, gender.Length)] + Stp[random.Next(0, Stp.Length)];
+                        my_dna = mydna.ToCharArray();
+                    }
+                    else
+                    {
+                        flag2 = false;
+                    }
+                }
+            }
+            else if (op == "male" || op == "m")
+            {
+                while (flag2)
+                {
+                    if (!(((my_dna[4] == 'A') || (my_dna[4] == 'T')) && ((my_dna[7] == 'G') || (my_dna[7] == 'C'))) && (!(((my_dna[4] == 'G') || (my_dna[4] == 'C')) && ((my_dna[7] == 'T') || (my_dna[7] == 'A')))))
+                    {
+                        mydna = "AGT" + gender[random.Next(0, gender.Length)] + gender[random.Next(0, gender.Length)] + Stp[random.Next(0, Stp.Length)];
+                        my_dna = mydna.ToCharArray();
+                    }
+                    else
+                    {
+                        flag2 = false;
+                    }
+                }
+            }
+            else
+            {
+                bool flag_3 = true;
+                while (flag_3)
+                {
+                    if (!((op == "male" || op == "female") || (op == "m" || op == "f")))
+                    {
+                        Console.WriteLine("Please write male (m) or female (f) for the operation ");
+                        op = Console.ReadLine();
+                    }
+                    else
+                    {
+                        flag_3 = false;
+                    }
+                }
+
+            }
+            string[] gen_op = new string[53];
+
+            for (int i = 0; i < my_dna.Length - 2; i = i + 3)
+            {
+                gen_op[i] = Convert.ToString(my_dna[i]) + Convert.ToString(my_dna[i + 1]) + Convert.ToString(my_dna[i + 2]);
+            }
+            int j = random.Next(1, 7);
+            string add_dna, add_dna1;
+            string[] a_acid = { Ala[random.Next(0, 4)], Arg[random.Next(0, 6)], Asn[random.Next(0, 2)], Asp[random.Next(0, 2)], Cys[random.Next(0, 2)], Gln[random.Next(0, 2)], Glu[random.Next(0, 2)], Gly[random.Next(0, 4)], His[random.Next(0, 2)], Ile[random.Next(0, 3)], Leu[random.Next(0, 6)], Lys[random.Next(0, 2)], Phe[random.Next(0, 2)], Pro[random.Next(0, 4)], Ser[random.Next(0, 6)], Thr[random.Next(0, 4)], Trp[random.Next(0, 1)], Tyr[random.Next(0, 2)], Val[random.Next(0, 4)] };
+            for (int k = 0; k < j; k = k + 1)
+            {
+                add_dna1 = null;
+                string add_dna2 = null;
+                int h = random.Next(1, 7);
+                for (int z = 0; z < h; z = z + 1)
+                {
+                    add_dna = a_acid[random.Next(1, 18)];
+                    add_dna1 = add_dna1 + add_dna;
+                    add_dna = null;
+                }
+                add_dna2 = "ATG" + add_dna1 + Stp[random.Next(0, 2)];
+                mydna = mydna + add_dna2;
+            }
+            my_dna = mydna.ToCharArray();
+            int a = 0;
+            string final;
+            //Console.WriteLine(my_dna);
+            string[] ch_final = new string[100];
+            for (int i = 0; i < my_dna.Length - 2; i = i + 3)
+            {
+                final = Convert.ToString(my_dna[i]) + Convert.ToString(my_dna[i + 1]) + Convert.ToString(my_dna[i + 2]);
+                ch_final[a] = final;
+                //Console.Write(ch_final[a] +" ");
+                a++;
+            }
+            return mydna;
+        }
+
         static void Main(string[] args)
         {
             string first_choise_string, second_choise_string, codon, temp = "";
             int first_choise_int = 0, second_choise_int = 0, beg = 0;
-            bool first_choise = true, second_choise = true, two_error = true, gene_structure = true , flag = true;
+            bool first_choise = true, second_choise = true, two_error = true, gene_structure = true, flag = true;
             string DNA_text, dna = null;
 
             ///////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -43,6 +164,7 @@ namespace MarsGen
             string[] Stp = { "TAA", "TGA", "TAG" }; //STOP
             string[] gender = { Lys[0], Phe[0], Gly[3], Pro[1] };
             ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            
             while (first_choise == true)
             {
                 Console.WriteLine("Welcome to Project MarsGen!\nPlease select one of these to continue:");
@@ -79,97 +201,7 @@ namespace MarsGen
                         break;
 
                     case 3:
-                        string mydna;
-                        Random random = new Random();
-                        random.Next(0, 5);
-                        Console.WriteLine("please enter the gender operation you want ");
-                        string op = Console.ReadLine();
-                        mydna = "AGT" + gender[random.Next(0, gender.Length)] + gender[random.Next(0, gender.Length)] + Stp[random.Next(0, Stp.Length)];
-                        char[] my_dna = mydna.ToCharArray();
-                        bool flag2 = true;
-                        if (op == "female")
-                        {
-                            while (flag2)
-                            {
-                                if (!(((my_dna[4] == 'A') || (my_dna[4] == 'T')) && ((my_dna[7] == 'A') || (my_dna[7] == 'T'))))
-                                {
-                                    mydna = "AGT" + gender[random.Next(0, gender.Length)] + gender[random.Next(0, gender.Length)] + Stp[random.Next(0, Stp.Length)];
-                                    my_dna = mydna.ToCharArray();
-                                }
-                                else
-                                {
-                                    flag2 = false;
-                                }
-                            }
-                        }
-                        else if (op == "male")
-                        {
-                            while (flag2)
-                            {
-                                if (!(((my_dna[4] == 'A') || (my_dna[4] == 'T')) && ((my_dna[7] == 'G') || (my_dna[7] == 'C'))) && (!(((my_dna[4] == 'G') || (my_dna[4] == 'C')) && ((my_dna[7] == 'T') || (my_dna[7] == 'A')))))
-                                {
-                                    mydna = "AGT" + gender[random.Next(0, gender.Length)] + gender[random.Next(0, gender.Length)] + Stp[random.Next(0, Stp.Length)];
-                                    my_dna = mydna.ToCharArray();
-                                }
-                                else
-                                {
-                                    flag2 = false;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            bool flag_3 = true;
-                            while (flag_3)
-                            {
-                                if (!(op == "male" || op == "female"))
-                                {
-                                    Console.WriteLine("pls write male or female for the operation ");
-                                    op = Console.ReadLine();
-                                }
-                                else
-                                {
-                                    flag_3 = false;
-                                }
-                            }
-
-                        }
-                        string[] gen_op = new string[53];
-                        Console.WriteLine();
-                        for (int i = 0; i < my_dna.Length - 2; i = i +3)
-                        {
-                            gen_op[i] = Convert.ToString(my_dna[i]) + Convert.ToString(my_dna[i + 1]) + Convert.ToString(my_dna[i + 2]);
-                        }
-                        int j = random.Next(1, 7);
-                        string add_dna , add_dna1;
-                        string[] a_acid = { Ala[random.Next(0, 4)], Arg[random.Next(0, 6)], Asn[random.Next(0, 2)], Asp[random.Next(0, 2)], Cys[random.Next(0, 2)], Gln[random.Next(0, 2)], Glu[random.Next(0, 2)], Gly[random.Next(0, 4)], His[random.Next(0, 2)], Ile[random.Next(0, 3)], Leu[random.Next(0, 6)], Lys[random.Next(0, 2)], Phe[random.Next(0, 2)], Pro[random.Next(0, 4)], Ser[random.Next(0, 6)], Thr[random.Next(0, 4)], Trp[random.Next(0, 1)], Tyr[random.Next(0, 2)], Val[random.Next(0, 4)] };
-                        for (int k = 0; k < j; k = k + 1)
-                        {
-                            add_dna1 = null;
-                            string add_dna2 = null;
-                            int h = random.Next(1, 7);
-                            for (int z = 0; z < h; z = z +1)
-                            {
-                                add_dna = a_acid[random.Next(1, 18)];
-                                add_dna1 = add_dna1 + add_dna;
-                                add_dna = null;
-                            }
-                            add_dna2 = "ATG" + add_dna1 + Stp[random.Next(0, 2)];
-                            mydna = mydna + add_dna2;
-                        }
-                        my_dna = mydna.ToCharArray();
-                        int a = 0;
-                        string final;
-                        Console.WriteLine(my_dna);
-                        string[] ch_final = new string[100];
-                        for (int i = 0; i < my_dna.Length - 2; i = i + 3)
-                        {
-                            final = Convert.ToString(my_dna[i]) + Convert.ToString(my_dna[i + 1]) + Convert.ToString(my_dna[i + 2]);
-                            ch_final[a] = final;
-                            Console.Write(ch_final[a] + " ");
-                            a++;
-                        }
-                        dna = mydna;
+                        dna=random_dna();
                         first_choise = false;
                         break;
                     default:
@@ -187,7 +219,7 @@ namespace MarsGen
             }
 
             string[] dna_codons = new string[(Dna.Length - 1) / 3];
-            Console.WriteLine(dna_codons.Length);
+            //Console.WriteLine(dna_codons.Length);
 
             //Console.WriteLine(Dna);
             for (int i = 0; i < Dna.Length; i++)
@@ -210,30 +242,31 @@ namespace MarsGen
                     dna_codons[k] = codon;
                     Console.Write(dna_codons[k] + " ");
                 }
+                Console.WriteLine(Dna[1]);
             }
             //////////////////////////////////////////////////////////////////////////////////////////////////
 
             while (second_choise == true)
             {
-                Console.WriteLine("\nYou entered your DNA\nPlease choose your operation:\n");
-                Console.WriteLine("1-Check DNA gene structure\n2-Check DNA of BLOB organism\n3-Produce complement of a DNA sequence\n4-Determine amino acids\n" +
-                    "5-Delete codons\n6-Insert codons\n7-Find codons\n8-Reverse codons\n9-Find the number of genes in a DNA strand\n10-Find the shortest gene\n" +
-                    "11-Find the longest gene\n12-Find the most repeated n-nucleotide sequence\n13-Hydrogen bond statistics for a DNA strand\n14-Simulate BLOB generations\n");
+                Console.WriteLine("\n\nYou entered your DNA\nPlease choose your operation:\n");
+                Console.WriteLine("4-Check DNA gene structure\n5-Check DNA of BLOB organism\n6-Produce complement of a DNA sequence\n7-Determine amino acids\n" +
+                    "8-Delete codons\n9-Insert codons\n10-Find codons\n11-Reverse codons\n12-Find the number of genes in a DNA strand\n13-Find the shortest gene\n" +
+                    "14-Find the longest gene\n15-Find the most repeated n-nucleotide sequence\n16-Hydrogen bond statistics for a DNA strand\n17-Simulate BLOB generations\n18-QUIT\n");
                 Console.Write("Your Operation:");
                 second_choise_string = Console.ReadLine();
                 if (int.TryParse(second_choise_string, out second_choise_int) == true) second_choise_int = Convert.ToInt32(second_choise_string);
                 else { second_choise = true; Console.Clear(); }
                 switch (second_choise_int)
                 {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
                     case 4:
                         break;
                     case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
                         Console.Write("DNA strand (stage 1) : " + " ");
                         for (int k = 0; k < 1; k++)
                         {
@@ -268,7 +301,7 @@ namespace MarsGen
                             }
                         }
                         break;
-                    case 6:
+                    case 9:
                         int mth_codon;
                         string added_codon;
 
@@ -304,10 +337,10 @@ namespace MarsGen
                                 Console.Write(" " + dna_added[k]);
                             }
                         }
-                        second_choise = false;
+                        second_choise = true;
                         break;
 
-                    case 7:
+                    case 10:
                         string find3;
                         int find_from;
                         Console.Write("Please enter codon to find:");
@@ -329,9 +362,9 @@ namespace MarsGen
                         //int count = Array.IndexOf(dna_codons, finding);
                         //Console.WriteLine(count);
 
-                        second_choise = false;
+                        second_choise = true;
                         break;
-                    case 8:
+                    case 11:
                         int reverse_from, reverse;
                         string[] reversed = dna_codons;
                         string[] reversed_dna = new string[dna_codons.Length];
@@ -355,26 +388,34 @@ namespace MarsGen
                         //for (int i = 0; i < dna_codons.Length; i++) Console.Write(reversed_dna[i] + " ");
 
 
-                        second_choise = false;
+                        second_choise = true;
                         break;
 
-                    case 9:
-                        second_choise = false;
-                        break;
-                    case 10:
-                        second_choise = false;
-                        break;
-                    case 11:
-                        second_choise = false;
-                        break;
                     case 12:
-                        second_choise = false;
+                        second_choise = true;
                         break;
                     case 13:
-                        second_choise = false;
+                        second_choise = true;
                         break;
                     case 14:
+                        second_choise = true;
+                        break;
+                    case 15:
+                        second_choise = true;
+                        break;
+                    case 16:
+
+                        second_choise = true;
+                        break;
+                    case 17:
+
+                        second_choise = true;
+                        break;
+                    case 18:
                         second_choise = false;
+                        Console.Clear();
+                        Console.WriteLine("Do you really want to quit?");
+                        //if()
                         break;
                     default:
                         second_choise = true;
